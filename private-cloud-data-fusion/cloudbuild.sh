442709dc0d22968326695a2b6f188626d30f53c9
sylvioneto <sylvio@google.com>
2023-01-20 13:17:50 -0300

    update copyrights to 2023

--- private-cloud-data-fusion/cloudbuild.sh
+++ private-cloud-data-fusion/cloudbuild.sh
@@ -1,4 +1,4 @@
-# Copyright 2022 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ set -e
 
 add_iam_member()
 {
-  gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=$1 --role=$2
+  gcloud projects add-iam-policy-binding $PROJECT_ID --member=$1 --role=$2
 }
 
 if [ -z "$GOOGLE_CLOUD_PROJECT" ]
@@ -25,17 +25,18 @@ then
    echo What Project Id do you want to deploy the solution to?
    read var_project_id
    gcloud config set project $var_project_id
-   export GOOGLE_CLOUD_PROJECT=$var_project_id
+   export PROJECT_ID=$var_project_id
+else
+   export PROJECT_ID=$GOOGLE_CLOUD_PROJECT
 fi
-
 
 if [ "$1" = "destroy" ]
 then
-    echo Destroying solution on project $GOOGLE_CLOUD_PROJECT
+    echo Destroying solution on project $PROJECT_ID
     gcloud builds submit . --config cloudbuild_destroy.yaml
 else
-    echo Deploying solution onto project $GOOGLE_CLOUD_PROJECT
-    BUCKET_NAME=gs://$GOOGLE_CLOUD_PROJECT-tf-state
+    echo Deploying solution onto project $PROJECT_ID
+    BUCKET_NAME=gs://$PROJECT_ID-tf-state
     if gsutil ls $BUCKET_NAME; then
         echo Terraform bucket already created!
     else
@@ -60,7 +61,7 @@ else
     sleep 30
 
     echo "Granting Cloud Build's Service Account IAM roles to deploy the resources..."
-    PROJECT_NUMBER=$(gcloud projects describe $GOOGLE_CLOUD_PROJECT --format='value(projectNumber)')
+    PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
     MEMBER=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com
     add_iam_member $MEMBER roles/editor
     add_iam_member $MEMBER roles/iam.securityAdmin
