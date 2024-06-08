c8d98a2f77964868c4df771c38c9733caaa79d43
ferraricharles <ferraricharles@google.com>
2024-06-08 09:12:18 +0100

    Remove flask debug mode

--- text-classification/code/main.py
+++ text-classification/code/main.py
@@ -64,7 +64,7 @@ def analyze_sentiment():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
 
 
     # https://lookerstudio.google.com/c/u/0/reporting/create?c.mode=edit&ds.connector=BIG_QUERY&ds.type=TABLE&ds.projectId=charles-hero&ds.datasetId=classified_messages&ds.tableId=classified_messages
