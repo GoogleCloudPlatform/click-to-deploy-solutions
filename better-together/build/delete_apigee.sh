40fb1e1401b0faa2333550fe417d40c4b6a877df
Fellipe Medeiros <femedeiros@google.com>
2024-07-29 11:09:49 -0300

    Updated to read from env variable

--- better-together/build/delete_apigee.sh
+++ better-together/build/delete_apigee.sh
@@ -4,12 +4,11 @@ apt-get install unzip jq -y
 curl -L https://raw.githubusercontent.com/apigee/apigeecli/main/downloadLatest.sh | sh 
 export PATH=$PATH:$HOME/.apigeecli/bin
 
-export PROJECT="hero-test-428818"
 export REGION="europe-west1"
 export APIGEE_ENVIRONMENT=test1
 
-gcloud config set project $PROJECT
+gcloud config set project $PROJECT_ID
 
-apigeecli apis undeploy -n AddressValidation-Service -e $APIGEE_ENVIRONMENT -o $PROJECT -t $(gcloud auth print-access-token)
+apigeecli apis undeploy -n AddressValidation-Service -e $APIGEE_ENVIRONMENT -o $PROJECT_ID -t $(gcloud auth print-access-token)
 
-apigeecli apis delete -n AddressValidation-Service -o $PROJECT -t $(gcloud auth print-access-token)
+apigeecli apis delete -n AddressValidation-Service -o $PROJECT_ID -t $(gcloud auth print-access-token)
