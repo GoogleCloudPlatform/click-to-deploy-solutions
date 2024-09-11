6a35094498be6d6f7f9ebff85cdcd0dfd0794bea
Fellipe Medeiros <femedeiros@google.com>
2024-09-11 10:46:01 +0000

    fix docai permissions

--- document-ai/infra/provider.tf
+++ document-ai/infra/provider.tf
@@ -18,7 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.59.0"
+      version = ">= 5.4.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.20.0"
     }
   }
   provider_meta "google" {
