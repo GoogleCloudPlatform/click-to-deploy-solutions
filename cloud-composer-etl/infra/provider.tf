8731a1d8e535c9eecc4b380e922cbdf41a8fceab
Sylvio <sylvio@google.com>
2024-10-15 20:52:20 +0000

    bump composer module to 5.1

--- cloud-composer-etl/infra/provider.tf
+++ cloud-composer-etl/infra/provider.tf
@@ -18,11 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4"
+      version = "~> 5.44"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4"
+      version = "~> 5.44"
     }
   }
   provider_meta "google" {
