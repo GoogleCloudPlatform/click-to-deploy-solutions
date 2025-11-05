38f9365e858084b788ff421f1366b4cdc8d967e3
David Lenehan <dlenehan@google.com>
2025-11-05 10:41:03 +0000

    enable compute on deploy

--- recaptcha-demo/deploy.sh
+++ recaptcha-demo/deploy.sh
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+gcloud services enable compute.googleapis.com 
 # This variable is used to rename files that may otherwise be overwritten 
 START=$(date +%s)
 
